bea4ebe42e32073fc1182c6161ab64fc49ab6486
Alexander Grund <Flamefire@users.noreply.github.com>
2022-03-29 13:10:36 +0200

    Improve patch application script
    - Always (try to) apply the patch
    - Report statistics at the end
    - Report patches where skip detection would fail
    
    Happened e.g. for the ADB root option patch.
    
    Change-Id: I7eb498dd70ce80cb5c44f086a9b807638043df06

--- patches/applyPatches.sh
+++ patches/applyPatches.sh
@@ -7,16 +7,37 @@ set -euo pipefail
 
 PATCH_ROOT="$ANDROID_BUILD_TOP/device/sony/lilac/patches"
 
+numApplied=0
+numSkipped=0
+numWarned=0
+
 for p in "$PATCH_ROOT/"*.patch; do
     patch_dir=$(head -n1 "$p" | grep "# PWD: " | awk '{print $NF}')
     if [[ "$patch_dir" == "" ]]; then
         echo "Faulty patch: $p"
         exit 1
     fi
-    if ! patch --dry-run --reverse --force -p1 -d "$patch_dir" --input "$p" > /dev/null; then
-        echo "Applying $(basename "$p") in $patch_dir"
-        patch --forward -r /dev/null --no-backup-if-mismatch -p1 -d "$patch_dir" --input "$p"
+
+    echo -n "Applying $(basename "$p") in $patch_dir : "
+    # If the reverse patch could be applied, then the patch was likely already applied
+    patch --reverse --force  -p1 -d "$patch_dir" --input "$p" --dry-run > /dev/null && applied=1 || applied=0
+    if out=$(patch --forward -p1 -d "$patch_dir" --input "$p" -r /dev/null --no-backup-if-mismatch 2>&1); then
+        echo "Done."
+        ((++numApplied))
+        # We applied the patch but could apply the reverse before, i.e. would detect it as already applied.
+        # This may happen for patches only deleting stuff where the reverse (adding it) may succeed via fuzzy match
+        if [[ $applied == 1 ]]; then
+            echo "WARNING: Skip detection will not work correctly for this patch!"
+            ((++numWarned))
+        fi
+    elif [[ $applied == 1 ]]; then
+        echo "Skipped."
+        ((++numSkipped))
+    else
+        echo "Failed!"
+        echo "$out"
+        exit 1
     fi
 done
 
-echo "Done."
+echo "Patching done! Applied: $numApplied, skipped: $numSkipped, warnings: $numWarned"
