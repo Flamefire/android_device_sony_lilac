7860c009dea978cb970516fd348af814cf2092eb
Alexander Grund <Flamefire@users.noreply.github.com>
2022-05-20 19:00:29 +0200

    Add coloring to applyPatches output
    Change-Id: Ic4d8261ff936c98f0016c2e228429b1db7e307ae

--- patches/applyPatches.sh
+++ patches/applyPatches.sh
@@ -5,6 +5,11 @@
 
 set -euo pipefail
 
+GREEN='\033[0;32m'
+LGREEN='\033[1;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
 PATCH_ROOT="$ANDROID_BUILD_TOP/device/sony/lilac/patches"
 
 numApplied=0
@@ -18,26 +23,26 @@ for p in "$PATCH_ROOT/"*.patch; do
         exit 1
     fi
 
-    echo -n "Applying $(basename "$p") in $patch_dir : "
+    echo -n "Applying $(basename "$p") in ${patch_dir}: "
     # If the reverse patch could be applied, then the patch was likely already applied
     patch --reverse --force  -p1 -d "$patch_dir" --input "$p" --dry-run > /dev/null && applied=1 || applied=0
     if out=$(patch --forward -p1 -d "$patch_dir" --input "$p" -r /dev/null --no-backup-if-mismatch 2>&1); then
-        echo "Done."
+        echo -e "${LGREEN}Done.${NC}"
         ((++numApplied))
         # We applied the patch but could apply the reverse before, i.e. would detect it as already applied.
         # This may happen for patches only deleting stuff where the reverse (adding it) may succeed via fuzzy match
         if [[ $applied == 1 ]]; then
-            echo "WARNING: Skip detection will not work correctly for this patch!"
+            echo -e "${YELLOW}WARNING${NC}: Skip detection will not work correctly for this patch!"
             ((++numWarned))
         fi
     elif [[ $applied == 1 ]]; then
-        echo "Skipped."
+        echo -e "${GREEN}Skipped.${NC}"
         ((++numSkipped))
     else
-        echo "Failed!"
+        echo -e "${RED}Failed!${NC}"
         echo "$out"
         exit 1
     fi
 done
 
-echo "Patching done! Applied: $numApplied, skipped: $numSkipped, warnings: $numWarned"
+echo -e "Patching done! ${LGREEN}Applied: ${numApplied}${NC}, ${GREEN}skipped: ${numSkipped}${NC}, ${YELLOW}warnings: ${numWarned}${NC}"
